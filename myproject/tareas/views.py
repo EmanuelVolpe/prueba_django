4bc5bfc4a8cc752b9f2047df7732b6f9ce74c64f
EmanuelVolpe <emanuelvolpe@gmail.com>
2022-07-05 23:55:02 -0300

    agrega validacion de formulario

--- myproject/tareas/views.py
+++ myproject/tareas/views.py
@@ -1,8 +1,17 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 
 tareas = ["foo", "bar", "baz"]
+
+
+class FormularioNuevaTarea(forms.Form):
+    tarea = forms.CharField(label="Nueva Tarea")
+    prioridad = forms.IntegerField(
+        label="prioridad", min_value=1, max_value=10)
 
 # Create your views here.
 
@@ -17,6 +26,31 @@ def index(request):
 
 
 def agregar(request):
+    # Check if method is POST
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = FormularioNuevaTarea(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            tarea = form.cleaned_data["tarea"]
+
+            # Add the new task to our list of tasks
+            tareas.append(tarea)
+
+            # Redirect user to list of tasks
+            return HttpResponseRedirect(reverse("index"))
+
+        else:
+            # If the form is invalid, re-render the page with existing information.
+            return render(request, "tasks/add.html", {
+                "titulo": "AGREGAR TAREA",
+                "form": form
+            })
     return render(request, "agregar.html", {
-        "titulo": "AGREGAR TAREA"
+        "titulo": "AGREGAR TAREA",
+        "form": FormularioNuevaTarea()
     })
